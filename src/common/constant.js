d360c047b9e1668d86a985c43b5907e18d2824c9
Firmansyah Otoluwa <firmansyaho@primelab.io>
2022-06-09 01:47:11 +0700

    Revert "Fix api request error"
    This reverts commit 988afbe31188f980cb54dcfd391bf824a323c60e.

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,2 +1,2 @@
 export const HASH_BLOCK_URL = 'http://localhost:8080';
-export const TRANSACTION_URL = 'https://blockchain-server-http-alb-83623561.us-east-1.elb.amazonaws.com';
+export const TRANSACTION_URL = 'http://blockchain-server-http-alb-83623561.us-east-1.elb.amazonaws.com';
